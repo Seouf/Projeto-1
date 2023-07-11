347888cee5231f550ca25efa3b432364a89a4c31
Seouf <swoof_aranda@outlook.com>
2023-07-11 10:57:40 -0300

    Few SQL Infos / City Class / Main Page Base

--- Pages/login.php
+++ Pages/login.php
@@ -1,31 +1,20 @@
-
 <!DOCTYPE html>
-<html>
-<head>
-  <title>Login</title>
-</head>
-<body>
-  <h1>Login</h1>
-
-  <form method="POST" action="">
-    <label for="username">Username:</label>
-    <input type="text" name="username" required><br>
-    <label for="password">Password:</label>
-    <input type="password" name="password" required><br>
-    <input type="submit" value="Log in">
-  </form>
-  <?php
+ <?php
+  session_start();
+    session_unset();
+    session_destroy();
+    session_start();
           include_once("../Classes/Database.php");
           include_once("../Classes/Login.php");
 
           if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username = $_POST['username'];
             $password = $_POST['password'];
-            $user = New User($username);
           
             $DbUser = User::DbByUsername($username);
           
             if (password_verify($password, $DbUser['user_password_digest'])){
+            $user = New User($username);
             $user->login();
             header('Location: main.php');
             exit();
@@ -34,6 +23,27 @@
             }
           }
     ?>
+    
+<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <h1>Login</h1>
+
+  <form method="POST" action="">
+    <label for="loginuserinput">Username:</label>
+    <input type="text" id="loginuserinput" name="username" autocomplete="username" required><br> 
+    <label for="passworduserinput">Password:</label>
+    <input type="password" id="passworduserinput" name="password" autocomplete="current-password" required><br>
+    <input type="submit" value="Log in">
+  </form>
+ <?php  
+ // referência autocomplete: https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/autocomplete#values 
+ // referência doctype HTML: https://developer.mozilla.org/en-US/docs/Web/HTML/Quirks_Mode_and_Standards_Mode
+ // referência request method: https://cursos.alura.com.br/forum/topico-uso-do-_server-request_method-251356
+
+ ?>
 </body>
 </html>
 
--- Pages/main.php
+++ Pages/main.php
@@ -2,10 +2,17 @@
 <?php
 include_once("../Classes/Database.php");
 include_once("../Classes/Login.php");
+include_once("../Classes/Cities.php");
 if (!User::isLoggedIn()){
     header('Location: login.php');
     exit();
 }
+$cities = City::getAllCities($db);
+//city_name
+//latitude
+//longitude
+//country_id
+//rating
 ?>
 <html>
     <head>
@@ -13,11 +20,21 @@ if (!User::isLoggedIn()){
     </head>
     <body>
         <div id="main-content">
-            <div id="navbar">
-            </div>
             <div id="side-bar">
             </div>
             <div id="content">
+                <?php foreach ($cities as $city){ ?>
+                <div class = "cities">
+                <h2> <a href='city.php?id=<?= $city->city_id ?>'> <?= $this->city_name ?> </a></h2>
+                <p>Country: <?= $this->country_id ?> </p>
+                <p>Latitude: <?= $this->latitude ?> </p>
+                <p>Longitude: <?= $this->longitude ?> </p>
+                <p>Rating: <span class="rating"> <?= $this->rating ?> </span></p>
+                <button onclick='rateCity("<?= $this->city_id ?>upvote")'>Upvote</button>
+                <button onclick='rateCity("<?= $this->city_id ?>downvote")'>Downvote</button>
+                <hr/>
+                </div>
+                <?php } ?>
             </div>
         </div>
     </body>
